89e0915c25756e41508241a7b6ef83c7541a85da
Beatrizayon <Beatriz.ayon04@gmail.com>
2025-11-15 03:36:53 -0800

    Add files via upload

---
+++ gamecode.js
@@ -0,0 +1,79 @@
+var ballx= 300;
+var bally= 300;
+var ballSize= 40;
+var score = 0;
+var gameState = "L1";
+
+function setup() {
+  createCanvas(600,600);
+  textAlign(CENTER);
+  textSize(20);
+}//end of setup
+
+function draw(){
+background(210,1,3);
+if(gameState == "L1"){
+  levelOne();
+}
+if(gameState == "L2"){
+  levelTwo();
+}
+if(gameState == "L3"){
+  levelThree();
+}
+  text(("Score: " + score),width/2,40); 
+
+}//end of draw
+
+function levelOne(){
+  text("Level 1", width/2, height-20);
+  var distToBall = dist(ballx,bally,mouseX,mouseY);
+  if(distToBall < ballSize/2){
+      ballx= random(width)
+      bally= random(height)
+      score = score + 1;
+  }
+  if(score>= 5){
+  gameState = "L2";
+  }
+  ellipse(ballx,bally,ballSize,ballSize);
+  line(ballx,bally,mouseX,mouseY);
+
+
+}//end of level 1
+
+function levelTwo(){
+  background(235,122,52);
+  text("Level 2", width/2, height-20);
+  var distToBall = dist(ballx,bally,mouseX,mouseY);
+  if(distToBall < ballSize/2){
+      ballx= random(width)
+      bally= random(height)
+      score = score + 1;
+  }
+  if(score>= 10){
+    gameState= "L3";
+  }
+  ellipse(ballx,bally,ballSize,ballSize);
+  //line(ballx,bally,mouseX,mouseY);
+
+
+}//end of level 2
+
+function levelThree(){
+  background(235, 216, 52);
+  text("Level 3", width/2, height-20);
+  var distToBall = dist(ballx,bally,mouseX,mouseY);
+  if(distToBall < ballSize/2){
+      ballx= random(width)
+      bally= random(height)
+      score = score + 1;
+      ballSize= ballSize - .5;
+  }
+  if(score>= 15){
+  }
+  ellipse(ballx,bally,ballSize,ballSize);
+  //line(ballx,bally,mouseX,mouseY);
+
+
+}//end of level 3
